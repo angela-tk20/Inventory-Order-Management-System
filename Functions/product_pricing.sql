738f52778ab0ec2764738c443ba45f1456ecd75a
angela-tk20 <angela.tk3164@gmail.com>
2025-11-26 13:23:24 +0000

    Remove comments

--- Functions/product_pricing.sql
+++ Functions/product_pricing.sql
@@ -11,12 +11,12 @@ DETERMINISTIC
 BEGIN
     DECLARE v_price DECIMAL(10,2);
     
-    -- Get current price
+   
     SELECT price 
     INTO v_price
     FROM products
     WHERE product_id = p_product_id;
     
-    -- Return price (will be NULL if product doesn't exist)
+    
     RETURN v_price;
 END $$
